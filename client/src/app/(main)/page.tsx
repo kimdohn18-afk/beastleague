2760ce906e65787ae8e2f2c236af71cde4c771eb
kimdohn18-afk <kimdohn18@gmail.com>
2026-04-07 17:51:49 +0900

    Update page.tsx

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { requestFcmToken } from '@/lib/firebase';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
