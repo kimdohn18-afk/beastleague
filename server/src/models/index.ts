cf8b51f58d266e8ff1d905421bf34704335366c5
kimdohn18-afk <kimdohn18@gmail.com>
2026-04-27 15:50:31 +0900

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,7 @@
+export { User } from './User';
 export { Character } from './Character';
 export { Game } from './Game';
 export { Placement } from './Placement';
-export { User } from './User';
 export { GuestBook } from './GuestBook';
 export { Feed } from './Feed';
 export { Like } from './Like';
